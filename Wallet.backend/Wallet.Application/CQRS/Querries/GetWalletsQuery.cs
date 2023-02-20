bf8dcccd5bdba7d001c1c2611f1bdb8c76015604
Salov-it <salovit96@gmail.com>
2023-02-20 12:51:25 +0300

    сделал получения всех кошельков

--- Wallet.backend/Wallet.Application/CQRS/Querries/GetWalletsQuery.cs
+++ Wallet.backend/Wallet.Application/CQRS/Querries/GetWalletsQuery.cs
@@ -5,5 +5,6 @@ namespace WalletService.Application.CQRS.Querries
 {
     public class GetWalletsQuery : IRequest<List<Wallet>>
     {
+        public List<Wallet> WalletList { get; set; }
     }
 }
---
+++ Wallet.backend/Wallet.Application/CQRS/Querries/GetWalletsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using WalletService.Application.Interface;
+using WalletService.Domain;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using AutoMapper;
+
+namespace WalletService.Application.CQRS.Querries
+{
+    public class GetWalletsQueryHandler : IRequestHandler<GetWalletsQuery, List<Wallet>>
+    {
+        private IWalletContext _context;
+    
+        public GetWalletsQueryHandler(IWalletContext context)
+        {
+            _context = context;
+        }
+      
+        public async Task<List<Wallet>> Handle(GetWalletsQuery request, CancellationToken cancellationToken)
+        {
+          var content = _context.Wallets.ToList();
+
+            if(content == null )
+            {
+                return null;
+            }
+
+            return content;
+        }
+    }
+}
